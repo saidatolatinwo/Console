505ba44bd100a21cb0b120ef9275dce5c603bcc6
saidatolatinwo <saidatolatinwo2020@gmail.com>
2022-09-26 13:00:30 +0100

    created the html file

---
+++ Assignment.js
@@ -0,0 +1,88 @@
+let number = 5
+
+let remainder = (number % 2)
+
+if (remainder == 0 )
+{
+    console.log(`${number} is even`)
+}
+else{
+    console.log(`${number} is Odd`)
+}
+
+let Number = 8
+
+let Remainder = (Number % 2)
+
+if (Remainder == 0 )
+{
+    console.log(`${Number} is even`)
+}
+else{
+    console.log(`${Number} is Odd`)
+}
+
+let x=5;
+let y=6;
+
+// y>x
+if (x>y)
+
+{
+    console.log("x is the greatest")
+}
+else
+{
+    console.log("y is the greatest")
+}
+
+let X=10
+let Y=20
+let Z=30
+
+// Z>Y>X
+    console.log(X>Y)
+    console.log(X>Z)
+    console.log(Y>X)
+    console.log(Y>Z)
+    console.log(Z>X)
+    console.log(Z>Y)
+    console.log(Z<Y)
+    console.log(X>=Y>=Z)
+    console.log(Y>=Z>=X)
+    console.log(Z>=Y>=X)
+
+let a=10
+let b=20
+let c=30
+
+// a<b<c
+
+if (a>b<=c)
+{
+    console.log("a is greater than b and less than or equal to c")
+}
+else
+{
+    console.log("a is less than b and not less than or equal to c")   
+}
+let votingAge = 50;
+if (votingAge >= 20)
+{
+    console.log("not eligible to vote")
+}
+else (votingAge >= 30)
+{
+console.log("eligible to vote")
+}
+
+
+let drivingAge = 18;
+if (drivingAge <= 18)
+{
+    console.log("not eligible to drive")
+}
+else (drivingAge >= 18)
+{
+console.log("eligible to vote")
+}
